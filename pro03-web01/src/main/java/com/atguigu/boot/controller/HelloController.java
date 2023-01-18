3df67b4a6df0b790dc63e5b909a77b4d4d502f61
bangbangbangdi <46771602+bangbangbangdi@users.noreply.github.com>
2023-01-18 20:12:05 +0900

    zz

--- pro03-web01/src/main/java/com/atguigu/boot/controller/HelloController.java
+++ pro03-web01/src/main/java/com/atguigu/boot/controller/HelloController.java
@@ -1,8 +1,11 @@
 package com.atguigu.boot.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -68,5 +71,6 @@ public class HelloController {
         map.put("empage",empAge);
         return map;
     }
+
 
 }
--- pro03-web01/src/main/java/com/atguigu/boot/controller/RequestController.java
+++ pro03-web01/src/main/java/com/atguigu/boot/controller/RequestController.java
@@ -1,12 +1,15 @@
 package com.atguigu.boot.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,22 +17,43 @@ import java.util.Map;
 public class RequestController {
 
     @GetMapping("/goto")
-    public String gotoPage(HttpServletRequest request){
-        request.setAttribute("msg","success");
-        request.setAttribute("code",200);
+    public String gotoPage(HttpServletRequest request) {
+        request.setAttribute("msg", "success");
+        request.setAttribute("code", 200);
         return "forward:success";
+    }
+
+    @GetMapping("/params")
+    public String testParam(Map<String, Object> map,
+                            Model model,
+                            HttpServletRequest request,
+                            HttpServletResponse response) {
+        map.put("hello", "world777");
+        model.addAttribute("world", "hello777");
+        request.setAttribute("message", "HelloWorld");
+        Cookie cookie = new Cookie("c1", "v1");
+        response.addCookie(cookie);
+        return "forward:/success";
     }
 
     @GetMapping("/success")
     @ResponseBody
-    public Map success(@RequestAttribute("msg") String msg,
-                       @RequestAttribute("code") Integer code,
-                       HttpServletRequest request){
-        Map<String,Object> map = new HashMap<>();
+    public Map success(@RequestAttribute(value = "msg",required = false) String msg,
+                       @RequestAttribute(value = "code",required = false) Integer code,
+                       HttpServletRequest request) {
+        Map<String, Object> map = new HashMap<>();
         Object msg1 = request.getAttribute("msg");
-        map.put("annotationMsg",msg);
-        map.put("annotationCode",code);
-        map.put("requestMsg",msg1);
+        Object hello = request.getAttribute("hello");
+        Object world = request.getAttribute("world");
+        Object message = request.getAttribute("message");
+
+        map.put("annotationMsg", msg);
+        map.put("annotationCode", code);
+        map.put("requestMsg", msg1);
+
+        map.put("hello", hello);
+        map.put("world", world);
+        map.put("message",message);
         return map;
     }
 
