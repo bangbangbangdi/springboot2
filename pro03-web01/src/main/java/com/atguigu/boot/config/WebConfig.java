b6a547123dceb37e53e40cdd61e4ef63127bc80d
bangbangbangdi <46771602+bangbangbangdi@users.noreply.github.com>
2023-01-24 19:46:59 +0900

    zz

--- pro03-web01/src/main/java/com/atguigu/boot/config/WebConfig.java
+++ pro03-web01/src/main/java/com/atguigu/boot/config/WebConfig.java
@@ -1,7 +1,11 @@
 package com.atguigu.boot.config;
 
+import com.atguigu.boot.bean.Pet;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.util.UrlPathHelper;
@@ -26,6 +30,24 @@ public class WebConfig /*implements WebMvcConfigurer*/ {
                 UrlPathHelper urlPathHelper = new UrlPathHelper();
                 urlPathHelper.setRemoveSemicolonContent(false);
                 configurer.setUrlPathHelper(urlPathHelper);
+            }
+
+            @Override
+            public void addFormatters(FormatterRegistry registry) {
+                registry.addConverter(new Converter<String, Pet>() {
+                    @Override
+                    public Pet convert(String source) {
+                        if (!StringUtils.isEmpty(source)){
+                            Pet pet = new Pet();
+                            String[] split = source.split(",");
+                            pet.setName(split[0]);
+                            pet.setAge(split[1]);
+                            return pet;
+                        }
+                        System.out.println("source is null");
+                        return null;
+                    }
+                });
             }
         };
     }
---
+++ pro03-web01/src/main/java/com/atguigu/boot/controller/ResponseTestController.java
@@ -0,0 +1,23 @@
+package com.atguigu.boot.controller;
+
+import com.atguigu.boot.bean.Person;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
+
+@Controller
+public class ResponseTestController {
+
+    @ResponseBody
+    @GetMapping("/test/person")
+    public Person getPerson(){
+        Person person = new Person();
+        person.setAge(20);
+        person.setBirth(new Date());
+        person.setUserName("BangDi");
+        return person;
+    }
+
+}
