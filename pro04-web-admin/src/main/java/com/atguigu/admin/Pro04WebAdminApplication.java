5e473b8dd4642bec81195e53b71dc9db5a646b1f
bangbangbangdi <46771602+bangbangbangdi@users.noreply.github.com>
2023-01-26 02:10:47 +0900

    miemie

---
+++ pro04-web-admin/src/main/java/com/atguigu/admin/Pro04WebAdminApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pro04WebAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Pro04WebAdminApplication.class, args);
+    }
+
+}
