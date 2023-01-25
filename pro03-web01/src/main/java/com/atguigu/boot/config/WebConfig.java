c8905ca1e8517c40ea04cffc9cbc22eb362bb9c2
bangbangbangdi <46771602+bangbangbangdi@users.noreply.github.com>
2023-01-25 23:58:44 +0900

    zz

--- pro03-web01/src/main/java/com/atguigu/boot/config/WebConfig.java
+++ pro03-web01/src/main/java/com/atguigu/boot/config/WebConfig.java
@@ -1,14 +1,25 @@
 package com.atguigu.boot.config;
 
 import com.atguigu.boot.bean.Pet;
+import com.atguigu.boot.converter.GuiguMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.util.StringUtils;
+import org.springframework.web.accept.HeaderContentNegotiationStrategy;
+import org.springframework.web.accept.ParameterContentNegotiationStrategy;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.util.UrlPathHelper;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Configuration(proxyBeanMethods = false)
 public class WebConfig /*implements WebMvcConfigurer*/ {
@@ -48,6 +59,28 @@ public class WebConfig /*implements WebMvcConfigurer*/ {
                         return null;
                     }
                 });
+            }
+
+            @Override
+            public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+                converters.add(new GuiguMessageConverter());
+            }
+
+            /*
+            * 自定义内容协商策略
+            * */
+            @Override
+            public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+                // Map<String,MediaType> mediaTypes
+                Map<String, MediaType> mediaTypeMap = new HashMap<>();
+                mediaTypeMap.put("json",MediaType.APPLICATION_JSON);
+                mediaTypeMap.put("xml",MediaType.APPLICATION_XML);
+                mediaTypeMap.put("gg",MediaType.parseMediaType("application/x-guigu"));
+                // 指定支持解析哪些参数对应的哪些媒体类型
+                ParameterContentNegotiationStrategy parameterStrategy = new ParameterContentNegotiationStrategy(mediaTypeMap);
+                //parameterStrategy.setParameterName();
+                HeaderContentNegotiationStrategy headerStrategy = new HeaderContentNegotiationStrategy();
+                configurer.strategies(Arrays.asList(parameterStrategy,headerStrategy));
             }
         };
     }
---
+++ pro03-web01/src/main/java/com/atguigu/boot/controller/ViewTestController.java
@@ -0,0 +1,18 @@
+package com.atguigu.boot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewTestController {
+
+    @GetMapping("/atguigu")
+    public String atguigu(Model model){
+
+        model.addAttribute("msg","Hello BangDi");
+        model.addAttribute("link","http://www.baidu.com");
+        return "success";
+    }
+
+}
