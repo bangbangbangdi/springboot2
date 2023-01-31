6e5fe2dd1a277552445cc5bcdcc0639e5c4f5a70
bangbangbangdi <46771602+bangbangbangdi@users.noreply.github.com>
2023-02-01 02:01:31 +0900

    miemie

--- pro04-web-admin/src/main/java/com/atguigu/admin/config/AdminWebConfig.java
+++ pro04-web-admin/src/main/java/com/atguigu/admin/config/AdminWebConfig.java
@@ -2,9 +2,11 @@ package com.atguigu.admin.config;
 
 import com.atguigu.admin.interceptor.LoginInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+//@EnableWebMvc
 @Configuration
 public class AdminWebConfig implements WebMvcConfigurer {
 
